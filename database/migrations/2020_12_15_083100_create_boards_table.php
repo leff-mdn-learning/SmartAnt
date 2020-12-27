d1f7db8f925757d7bf7ae02447e3e601626190cf
Alexandr Yakovlev <leffalt@gmail.com>
2020-12-27 14:27:13 +0700

    make migrations and model

--- database/migrations/2020_12_15_083100_create_boards_table.php
+++ database/migrations/2020_12_15_083100_create_boards_table.php
@@ -16,6 +16,11 @@ class CreateBoardsTable extends Migration
         Schema::create('boards', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('title');
+            $table->string('color');
+
+            $table->foreignId('owner_id')->constrained('users');
         });
     }
 
