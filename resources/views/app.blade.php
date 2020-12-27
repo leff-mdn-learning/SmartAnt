6272b16fec62df9a9e81cd318d1c49c6c7a2ce3f
Alexandr Yakovlev <leffalt@gmail.com>
2020-12-27 19:04:59 +0700

    57.3-51 vue don't registry error.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'SmartAnt') }}</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body class="h-screen text-gray-800">
+    <div id="app">
+        <Board></Board>
+    </div>
+</body>
+
+</html>
