a9428cb3a56a048dfebaee2dcb7ff323eb8413dc
Alexandr Yakovlev <leffalt@gmail.com>
2021-01-02 12:42:11 +0700

    add element to board

--- resources/js/Board.vue
+++ resources/js/Board.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="h-full flex flex-col items-stretch bg-purple-500">
+        <div class="text-white flex justify-between items-center mb-2">
+            <div>x</div>
+            <div>x</div>
+            <div>x</div>
+        </div>
     </div>
 </template>
 
